63dc2b4d87e02c53a101bf691d30ce24fae50923
hudhaifagburi2 <hudhaifagburi@gmail.com>
2026-01-13 10:34:13 +0300

    fix endpoints related to admin registration and student

--- src/TransportationAttendance.Domain/Authorization/Roles.cs
+++ src/TransportationAttendance.Domain/Authorization/Roles.cs
@@ -4,6 +4,7 @@ public static class Roles
 {
     public const string Student = "Student";
     public const string Staff = "Staff";
+    public const string Driver = "Driver";
     public const string Supervisor = "Supervisor";
     public const string Admin = "Admin";
     public const string SuperAdmin = "SuperAdmin";
@@ -11,7 +12,8 @@ public static class Roles
 
     public static readonly string[] AdminRoles = { Admin, SuperAdmin, SystemAdministrator };
     public static readonly string[] StaffRoles = { Staff, Supervisor, Admin, SuperAdmin, SystemAdministrator };
+    public static readonly string[] DriverRoles = { Driver, Supervisor, Admin, SuperAdmin, SystemAdministrator };
     public static readonly string[] SupervisorAndAbove = { Supervisor, Admin, SuperAdmin, SystemAdministrator };
     public static readonly string[] SuperAdminOnly = { SuperAdmin, SystemAdministrator };
-    public static readonly string[] AllRoles = { Student, Staff, Supervisor, Admin, SuperAdmin, SystemAdministrator };
+    public static readonly string[] AllRoles = { Student, Staff, Driver, Supervisor, Admin, SuperAdmin, SystemAdministrator };
 }
