0b3fa6090f91ce58c54d8c86ead5cd01201fbc0c
hudhaifagburi2 <hudhaifagburi@gmail.com>
2026-01-18 10:20:58 +0300

    phase 2: pwa bus management

--- src/TransportationAttendance.Application/Validators/CreateBusValidator.cs
+++ src/TransportationAttendance.Application/Validators/CreateBusValidator.cs
@@ -13,20 +13,15 @@ public class CreateBusValidator : AbstractValidator<CreateBusDto>
             .NotEmpty().WithMessage("رقم اللوحة مطلوب")
             .MaximumLength(20).WithMessage("رقم اللوحة يجب ألا يتجاوز 20 حرف");
 
+        RuleFor(x => x.BusNumber)
+            .MaximumLength(10).WithMessage("رقم الباص يجب ألا يتجاوز 10 أحرف")
+            .When(x => !string.IsNullOrEmpty(x.BusNumber));
+
         RuleFor(x => x.PeriodId)
             .GreaterThan(0).WithMessage("الفترة مطلوبة");
 
         RuleFor(x => x.Capacity)
             .InclusiveBetween(1, 100).WithMessage("السعة يجب أن تكون بين 1 و 100");
-
-        RuleFor(x => x.DriverName)
-            .MaximumLength(200).WithMessage("اسم السائق يجب ألا يتجاوز 200 حرف")
-            .When(x => !string.IsNullOrEmpty(x.DriverName));
-
-        RuleFor(x => x.DriverPhoneNumber)
-            .MaximumLength(20).WithMessage("رقم الهاتف يجب ألا يتجاوز 20 رقم")
-            .Matches(@"^[\d\s\+\-]+$").WithMessage("رقم الهاتف يجب أن يحتوي على أرقام فقط")
-            .When(x => !string.IsNullOrEmpty(x.DriverPhoneNumber));
     }
 }
 
@@ -40,19 +35,14 @@ public class UpdateBusValidator : AbstractValidator<UpdateBusDto>
             .NotEmpty().WithMessage("رقم اللوحة مطلوب")
             .MaximumLength(20).WithMessage("رقم اللوحة يجب ألا يتجاوز 20 حرف");
 
+        RuleFor(x => x.BusNumber)
+            .MaximumLength(10).WithMessage("رقم الباص يجب ألا يتجاوز 10 أحرف")
+            .When(x => !string.IsNullOrEmpty(x.BusNumber));
+
         RuleFor(x => x.PeriodId)
             .GreaterThan(0).WithMessage("الفترة مطلوبة");
 
         RuleFor(x => x.Capacity)
             .InclusiveBetween(1, 100).WithMessage("السعة يجب أن تكون بين 1 و 100");
-
-        RuleFor(x => x.DriverName)
-            .MaximumLength(200).WithMessage("اسم السائق يجب ألا يتجاوز 200 حرف")
-            .When(x => !string.IsNullOrEmpty(x.DriverName));
-
-        RuleFor(x => x.DriverPhoneNumber)
-            .MaximumLength(20).WithMessage("رقم الهاتف يجب ألا يتجاوز 20 رقم")
-            .Matches(@"^[\d\s\+\-]+$").WithMessage("رقم الهاتف يجب أن يحتوي على أرقام فقط")
-            .When(x => !string.IsNullOrEmpty(x.DriverPhoneNumber));
     }
 }
