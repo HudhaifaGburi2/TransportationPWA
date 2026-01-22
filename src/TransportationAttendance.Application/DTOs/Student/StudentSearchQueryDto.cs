e56ce2f53ffe0b4ba302089c3370647a8eece813
hudhaifagburi2 <hudhaifagburi@gmail.com>
2026-01-22 17:20:52 +0300

    MosaidBot Rag

--- src/TransportationAttendance.Application/DTOs/Student/StudentSearchQueryDto.cs
+++ src/TransportationAttendance.Application/DTOs/Student/StudentSearchQueryDto.cs
@@ -10,14 +10,3 @@ public record StudentSearchQueryDto
     public int PageNumber { get; init; } = 1;
     public int PageSize { get; init; } = 50;
 }
-
-public record PagedResult<T>
-{
-    public IReadOnlyList<T> Items { get; init; } = new List<T>();
-    public int TotalCount { get; init; }
-    public int PageNumber { get; init; }
-    public int PageSize { get; init; }
-    public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
-    public bool HasPreviousPage => PageNumber > 1;
-    public bool HasNextPage => PageNumber < TotalPages;
-}
